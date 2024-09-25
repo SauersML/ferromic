de4449874d1bd3fa070a343042fc184ad0bae271
ScottSauers <123714540+ScottSauers@users.noreply.github.com>
2024-09-25 16:34:09 -0500

    Update stats.rs

--- src/stats.rs
+++ src/stats.rs
@@ -832,7 +832,7 @@ fn process_vcf(
                             "{}",
                             format!("process_vcf: Consumer Thread {} started processing lines.", thread_id).cyan()
                         );
-                    } else if processed_count % 10 == 0 {
+                    } else if processed_count % 50 == 0 {
                         println!(
                             "{}",
                             format!(
