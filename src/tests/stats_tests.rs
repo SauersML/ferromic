1276bb2a5ff3b05d480ef13c44b7061e09bf89f3
ScottSauers <123714540+ScottSauers@users.noreply.github.com>
2024-11-03 18:04:14 -0600

    Update stats_tests.rs

--- src/tests/stats_tests.rs
+++ src/tests/stats_tests.rs
@@ -1087,14 +1087,6 @@ mod tests {
         let position_allele_map = Arc::new(Mutex::new(HashMap::new()));
         let chromosome = "1".to_string();
 
-        // Manually populate the position_allele_map
-        {
-            let mut pam = position_allele_map.lock();
-            pam.insert(1000, ('A', 'T'));
-            pam.insert(2000, ('A', 'A'));
-            pam.insert(3000, ('A', 'T'));
-        }
-
         let _result_group1 = process_variants(
             &variants,
             &sample_names,
@@ -1135,12 +1127,6 @@ mod tests {
         let mut _filtering_stats = FilteringStats::default();
         let seqinfo_storage = Arc::new(Mutex::new(Vec::new()));
         let position_allele_map = Arc::new(Mutex::new(HashMap::new()));
-        {
-            let mut pam = position_allele_map.lock();
-            pam.insert(1000, ('A', 'T'));
-            pam.insert(2000, ('A', 'T'));
-            pam.insert(3000, ('A', 'T'));
-        }
         let chromosome = "1".to_string();
     
         let _result_group1 = process_variants(
@@ -1187,14 +1173,6 @@ mod tests {
         let seqinfo_storage = Arc::new(Mutex::new(Vec::new()));
         let position_allele_map = Arc::new(Mutex::new(HashMap::new()));
         let chromosome = "1".to_string();
-
-        // Manually populate the position_allele_map
-        {
-            let mut pam = position_allele_map.lock();
-            pam.insert(1000, ('A', 'T'));
-            pam.insert(2000, ('A', 'A'));
-            pam.insert(3000, ('A', 'T'));
-        }
 
         let result = process_variants(
             &variants,
@@ -1322,12 +1300,7 @@ mod tests {
         let mut _filtering_stats = FilteringStats::default();
         let seqinfo_storage = Arc::new(Mutex::new(Vec::new()));
         let position_allele_map = Arc::new(Mutex::new(HashMap::new()));
-        {
-            let mut pam = position_allele_map.lock();
-            pam.insert(1000, ('A', 'T'));
-            pam.insert(2000, ('A', 'T'));
-            pam.insert(3000, ('A', 'T'));
-        }
+
         let chromosome = "1".to_string();
     
         let _result_group1 = process_variants(
