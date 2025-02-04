a09c67642b464b15e39ce3667dbf3aabb70b2bdc
SauersML <123714540+SauersML@users.noreply.github.com>
2025-02-04 01:19:01 -0600

    Update process.rs

--- src/process.rs
+++ src/process.rs
@@ -1176,7 +1176,7 @@ fn make_sequences(
             combined_cds_sequences.insert(name.clone(), Vec::new());
         }
 
-        let cds_start = std::cmp::max(cds_start - 1, region_start - 1); // 0-based.... or just seg_start??? Double check this.
+        let cds_start = std::cmp::max(cds_start - 1, region_start - 1); // 0-based.... or just cds_start??? Double check this.
         let cds_end = std::cmp::min(cds_end, region_end);
                 
         // After processing all segments, write one final .phy file
