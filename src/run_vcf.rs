79014c43ee4c6056cea19318a0a6b5bfaa24b475
SauersML <123714540+SauersML@users.noreply.github.com>
2025-03-09 03:13:00 -0500

    Update run_vcf.rs

--- src/run_vcf.rs
+++ src/run_vcf.rs
@@ -112,8 +112,7 @@ fn main() -> Result<(), VcfError> {
     if let Some(config_file) = args.config_file.as_ref() {
         log(LogLevel::Info, &format!("Config file provided: {}", config_file));
         let config_entries = parse_config_file(Path::new(config_file))?;
-        init_global_progress(config_entries.len());
-
+    
         let output_file = args
             .output_file
             .as_ref()
