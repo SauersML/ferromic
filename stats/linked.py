0184e89704bc11830d6520fba37aec9bacd4f588
SauersML <123714540+SauersML@users.noreply.github.com>
2025-09-07 23:45:06 -0500

    Update linked.py

--- stats/linked.py
+++ stats/linked.py
@@ -30,6 +30,11 @@ import traceback
 #PYTHONPATH="$TARGET:$PYTHONPATH" python3 /home/hsiehph/sauer354/di/ferromic/linked.py
 
 
+#PYTHONPATH="$TARGET:$PYTHONPATH" nohup setsid python3 -u /home/hsiehph/sauer354/di/ferromic/linked.py > linked.log 2>&1 & tail -f linked.log
+
+
+
+
 warnings.filterwarnings("ignore", category=FutureWarning)
 rng = np.random.default_rng(seed=42)
 
