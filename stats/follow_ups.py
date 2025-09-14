8871b90b311ce1d3cd3b4eeca6bb969505595b66
SauersML <123714540+SauersML@users.noreply.github.com>
2025-09-14 17:03:24 -0500

    Create follow_ups.py

---
+++ stats/follow_ups.py
@@ -0,0 +1,9 @@
+# chr6-76111919-INV-44661 E. coli
+
+# chr7-57835189-INV-284465 two breast cancer related and PMDD
+
+# chr6-141867315-INV-29159 Laryngitis and tracheitis
+
+# chr17-45585160-INV-706887 multiple skin conditions, obesity, multiple breast cancer-related, Diastolic heart failure, Mild cognitive impairment
+
+# chr12-46897663-INV-16289 eye inflammation, migraine, Rosacea
