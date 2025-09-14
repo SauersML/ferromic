d14bab375b71e0d8fef4eb322f752d8762e318c9
SauersML <123714540+SauersML@users.noreply.github.com>
2025-09-14 17:27:10 -0500

    Update follow_ups.py

--- stats/follow_ups.py
+++ stats/follow_ups.py
@@ -6,35 +6,48 @@
 
 
 Breast cancer indicators:
-"Has anyone in your family ever been diagnosed with the following cancer conditions? Think only of the people you are related to by blood. Select all that apply."
-"Who in your family has had breast cancer? Select all that apply."
-"Has a doctor or health care provider ever told you that you have or had any of the following cancers? Select all that apply."
-"Are you still seeing a doctor or health care provider for breast cancer?"
-"Have you or anyone in your family ever been diagnosed with the following cancer conditions? Think only of the people you are related to by blood. Select all that apply."
-"Including yourself, who in your family has had breast cancer? Select all that apply."
+Has anyone in your family ever been diagnosed with the following cancer conditions? Think only of the people you are related to by blood. Select all that apply.
+Who in your family has had breast cancer? Select all that apply.
+Has a doctor or health care provider ever told you that you have or had any of the following cancers? Select all that apply.
+Are you still seeing a doctor or health care provider for breast cancer?
+Have you or anyone in your family ever been diagnosed with the following cancer conditions? Think only of the people you are related to by blood. Select all that apply.
+Including yourself, who in your family has had breast cancer? Select all that apply.
+Are you currently prescribed medications and/or receiving treatment for breast cancer?
 
 Skin condition indicators:
-"Are you still seeing a doctor or health care provider for a skin condition (e.g., eczema, psoriasis)?"
-"Who in your family has had skin condition(s) (e.g., eczema, psoriasis)? Select all that apply."
-"Including yourself, who in your family has had skin condition(s) (e.g., eczema, psoriasis)? Select all that apply."
+Are you still seeing a doctor or health care provider for a skin condition (e.g., eczema, psoriasis)?
+Who in your family has had skin condition(s) (e.g., eczema, psoriasis)? Select all that apply.
+Including yourself, who in your family has had skin condition(s) (e.g., eczema, psoriasis)? Select all that apply.
+
+Skin cancer indicators:
+Who in your family has had skin cancer? Select all that apply.
+Are you still seeing a doctor or health care provider for skin cancer?
+Including yourself, who in your family has had skin cancer? Select all that apply.
 
 Obesity indicators:
-"Who in your family has had obesity? Select all that apply."
-"Are you still seeing a doctor or health care provider for obesity?"
-"Including yourself, who in your family has had obesity? Select all that apply."
+Who in your family has had obesity? Select all that apply.
+Are you still seeing a doctor or health care provider for obesity?
+Including yourself, who in your family has had obesity? Select all that apply.
 
 Heart failure indicators:
-"Who in your family has had congestive heart failure? Select all that apply."
-"Are you still seeing a doctor or health care provider for congestive heart failure?"
-"Including yourself, who in your family has had congestive heart failure? Select all that apply."
+Who in your family has had congestive heart failure? Select all that apply.
+Are you still seeing a doctor or health care provider for congestive heart failure?
+Including yourself, who in your family has had congestive heart failure? Select all that apply.
 
 Mild cognitive impairment:
-"Because of a physical, mental, or emotional condition, do you have serious difficulty concentrating, remembering or making decisions?"
-"Are you still seeing a doctor or health care provider for memory loss or impairment?"
-"Are you still seeing a doctor or health care provider for dementia?"
-"Who in your family has had dementia (includes Alzheimer's, vascular, etc.)? Select all that apply."
+Because of a physical, mental, or emotional condition, do you have serious difficulty concentrating, remembering or making decisions?
+Are you still seeing a doctor or health care provider for memory loss or impairment?
+Are you still seeing a doctor or health care provider for dementia?
+Who in your family has had dementia (includes Alzheimer's, vascular, etc.)? Select all that apply.
+About how old were you when you were first told you had dementia?
+Are you currently prescribed medications and/or receiving treatment for dementia?
+About how old were you when you were first told you had memory loss or impairment?
+Are you currently prescribed medications and/or receiving treatment for memory loss or impairment?
+Including yourself, who in your family has had dementia (includes Alzheimer's, vascular, etc.)? Select all that apply.
+Including yourself, who in your family has had memory loss or impairment? Select all that apply.
+Because of a physical, mental, or emotional condition, do you have difficulty doing errands alone such as visiting doctor's office or shopping?
 
 Migraine:
-"Who in your family has had migraine headaches? Select all that apply."
-"Are you still seeing a doctor or health care provider for migraine headaches?"
-"Including yourself, who in your family has had migraine headaches? Select all that apply."
+Who in your family has had migraine headaches? Select all that apply.
+Are you still seeing a doctor or health care provider for migraine headaches?
+Including yourself, who in your family has had migraine headaches? Select all that apply.
