2fecbfcc6c2b716efd90345e9a957e4f85a51ae6
SauersML <123714540+SauersML@users.noreply.github.com>
2025-09-14 17:21:15 -0500

    Update follow_ups.py

--- stats/follow_ups.py
+++ stats/follow_ups.py
@@ -1,7 +1,40 @@
-# chr7-57835189-INV-284465 two breast cancer related and PMDD
-
-# chr6-141867315-INV-29159 Laryngitis and tracheitis
+# chr7-57835189-INV-284465 two breast cancer related
 
 # chr17-45585160-INV-706887 multiple skin conditions, obesity, multiple breast cancer-related, Diastolic heart failure, Mild cognitive impairment
 
 # chr12-46897663-INV-16289 eye inflammation, migraine, Rosacea
+
+
+Breast cancer indicators:
+"Has anyone in your family ever been diagnosed with the following cancer conditions? Think only of the people you are related to by blood. Select all that apply."
+"Who in your family has had breast cancer? Select all that apply."
+"Has a doctor or health care provider ever told you that you have or had any of the following cancers? Select all that apply."
+"Are you still seeing a doctor or health care provider for breast cancer?"
+"Have you or anyone in your family ever been diagnosed with the following cancer conditions? Think only of the people you are related to by blood. Select all that apply."
+"Including yourself, who in your family has had breast cancer? Select all that apply."
+
+Skin condition indicators:
+"Are you still seeing a doctor or health care provider for a skin condition (e.g., eczema, psoriasis)?"
+"Who in your family has had skin condition(s) (e.g., eczema, psoriasis)? Select all that apply."
+"Including yourself, who in your family has had skin condition(s) (e.g., eczema, psoriasis)? Select all that apply."
+
+Obesity indicators:
+"Who in your family has had obesity? Select all that apply."
+"Are you still seeing a doctor or health care provider for obesity?"
+"Including yourself, who in your family has had obesity? Select all that apply."
+
+Heart failure indicators:
+"Who in your family has had congestive heart failure? Select all that apply."
+"Are you still seeing a doctor or health care provider for congestive heart failure?"
+"Including yourself, who in your family has had congestive heart failure? Select all that apply."
+
+Mild cognitive impairment:
+"Because of a physical, mental, or emotional condition, do you have serious difficulty concentrating, remembering or making decisions?"
+"Are you still seeing a doctor or health care provider for memory loss or impairment?"
+"Are you still seeing a doctor or health care provider for dementia?"
+"Who in your family has had dementia (includes Alzheimer's, vascular, etc.)? Select all that apply."
+
+Migraine:
+"Who in your family has had migraine headaches? Select all that apply."
+"Are you still seeing a doctor or health care provider for migraine headaches?"
+"Including yourself, who in your family has had migraine headaches? Select all that apply."
