6c1c896ebbdbc5c312711de096773e7a3bc119aa
SauersML <123714540+SauersML@users.noreply.github.com>
2025-11-16 15:41:05 -0600

    Create h2.py

---
+++ phewas/extra/h2.py
@@ -0,0 +1,26 @@
+Need to make .cov file:
+FID and IID (must match .fam).
+Remaining columns: all covariates
+- PCs
+-age
+- age^2
+-ancestry categories
+- sex
+
+
+need to make .pheno file:
+FID IID  disease_name
+id1 id1  1
+id2 id2  0
+id3 id3  0
+...
+
+Need to make .modelSnps file:
+Tells BOLT-REML which SNP goes into which variance component.
+format:
+columns are:
+SNP_ID    component_name
+
+We can do the normal SNP IDs.
+
+For inv calls, it cannot stay as an arbitrary float dosage for BOLT-REML. so we have to compute hard-calls. 
